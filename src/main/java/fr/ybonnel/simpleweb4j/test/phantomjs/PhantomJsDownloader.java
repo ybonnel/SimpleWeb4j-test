3cf32dcf60777b6510567e1c0bc55520021654cb
ybonnel <ybonnel@gmail.com>
2013-05-02 16:32:43 +0200

    Correct indent

--- src/main/java/fr/ybonnel/simpleweb4j/test/phantomjs/PhantomJsDownloader.java
+++ src/main/java/fr/ybonnel/simpleweb4j/test/phantomjs/PhantomJsDownloader.java
@@ -12,105 +12,105 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 class PhantomJsDownloader {
-  private final boolean isWindows;
-  private final boolean isMac;
-  private final boolean isLinux64;
+    private final boolean isWindows;
+    private final boolean isMac;
+    private final boolean isLinux64;
 
-  PhantomJsDownloader() {
-    String osName = System.getProperty("os.name");
-    isWindows = osName.startsWith("Windows");
-    isMac = osName.startsWith("Mac OS X") || osName.startsWith("Darwin");
-    isLinux64 = System.getProperty("sun.arch.data.model").equals("64");
-  }
-
-  public File downloadAndExtract() {
-    File installDir = new File(new File(System.getProperty("user.home")), ".phantomjstest");
-
-    String url;
-    File phantomJsExe;
-    if (isWindows) {
-      url = "http://phantomjs.googlecode.com/files/phantomjs-1.8.1-windows.zip";
-      phantomJsExe = new File(installDir, "phantomjs-1.8.1-windows/phantomjs.exe");
-    } else if (isMac) {
-      url = "http://phantomjs.googlecode.com/files/phantomjs-1.8.1-macosx.zip";
-      phantomJsExe = new File(installDir, "phantomjs-1.8.1-macosx/bin/phantomjs");
-    } else if (isLinux64) {
-      url = "http://phantomjs.googlecode.com/files/phantomjs-1.8.1-linux-x86_64.tar.bz2";
-      phantomJsExe = new File(installDir, "phantomjs-1.8.1-linux-x86_64/bin/phantomjs");
-    } else {
-      url = "http://phantomjs.googlecode.com/files/phantomjs-1.8.1-linux-i686.tar.bz2";
-      phantomJsExe = new File(installDir, "phantomjs-1.8.1-linux-i686/bin/phantomjs");
+    PhantomJsDownloader() {
+        String osName = System.getProperty("os.name");
+        isWindows = osName.startsWith("Windows");
+        isMac = osName.startsWith("Mac OS X") || osName.startsWith("Darwin");
+        isLinux64 = System.getProperty("sun.arch.data.model").equals("64");
     }
 
-    extractExe(url, installDir, phantomJsExe);
+    public File downloadAndExtract() {
+        File installDir = new File(new File(System.getProperty("user.home")), ".phantomjstest");
 
-    return phantomJsExe;
-  }
+        String url;
+        File phantomJsExe;
+        if (isWindows) {
+            url = "http://phantomjs.googlecode.com/files/phantomjs-1.8.1-windows.zip";
+            phantomJsExe = new File(installDir, "phantomjs-1.8.1-windows/phantomjs.exe");
+        } else if (isMac) {
+            url = "http://phantomjs.googlecode.com/files/phantomjs-1.8.1-macosx.zip";
+            phantomJsExe = new File(installDir, "phantomjs-1.8.1-macosx/bin/phantomjs");
+        } else if (isLinux64) {
+            url = "http://phantomjs.googlecode.com/files/phantomjs-1.8.1-linux-x86_64.tar.bz2";
+            phantomJsExe = new File(installDir, "phantomjs-1.8.1-linux-x86_64/bin/phantomjs");
+        } else {
+            url = "http://phantomjs.googlecode.com/files/phantomjs-1.8.1-linux-i686.tar.bz2";
+            phantomJsExe = new File(installDir, "phantomjs-1.8.1-linux-i686/bin/phantomjs");
+        }
 
-  private void extractExe(String url, File phantomInstallDir, File phantomJsExe) {
-    if (phantomJsExe.exists()) {
-      return;
+        extractExe(url, installDir, phantomJsExe);
+
+        return phantomJsExe;
     }
 
-    File targetZip = new File(phantomInstallDir, "phantomjs.zip");
-    downloadZip(url, targetZip);
+    private void extractExe(String url, File phantomInstallDir, File phantomJsExe) {
+        if (phantomJsExe.exists()) {
+            return;
+        }
 
-    System.out.println("Extracting phantomjs");
-    try {
-      if (isWindows) {
-        unzip(targetZip, phantomInstallDir);
-      } else if (isMac) {
-        new ProcessBuilder().command("/usr/bin/unzip", "-qo", "phantomjs.zip").directory(phantomInstallDir).start().waitFor();
-      } else {
-        new ProcessBuilder().command("tar", "-xjvf", "phantomjs.zip").directory(phantomInstallDir).start().waitFor();
-      }
-    } catch (Exception e) {
-      throw new IllegalStateException("Unable to unzip phantomjs from " + targetZip.getAbsolutePath());
-    }
-  }
+        File targetZip = new File(phantomInstallDir, "phantomjs.zip");
+        downloadZip(url, targetZip);
 
-  private void downloadZip(String url, File targetZip) {
-    if (targetZip.exists()) {
-      return;
-    }
-
-    System.out.println("Downloading phantomjs from " + url + "...");
-
-    File zipTemp = new File(targetZip.getAbsolutePath() + ".temp");
-    try {
-      zipTemp.getParentFile().mkdirs();
-
-      InputSupplier<InputStream> input = Resources.newInputStreamSupplier(URI.create(url).toURL());
-      OutputSupplier<FileOutputStream> ouput = Files.newOutputStreamSupplier(zipTemp);
-
-      ByteStreams.copy(input, ouput);
-    } catch (IOException e) {
-      throw new IllegalStateException("Unable to download phantomjs from " + url);
+        System.out.println("Extracting phantomjs");
+        try {
+            if (isWindows) {
+                unzip(targetZip, phantomInstallDir);
+            } else if (isMac) {
+                new ProcessBuilder().command("/usr/bin/unzip", "-qo", "phantomjs.zip").directory(phantomInstallDir).start().waitFor();
+            } else {
+                new ProcessBuilder().command("tar", "-xjvf", "phantomjs.zip").directory(phantomInstallDir).start().waitFor();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to unzip phantomjs from " + targetZip.getAbsolutePath());
+        }
     }
 
-    zipTemp.renameTo(targetZip);
-  }
+    private void downloadZip(String url, File targetZip) {
+        if (targetZip.exists()) {
+            return;
+        }
 
-  private static void unzip(File zip, File toDir) throws IOException {
-      try (ZipFile zipFile = new ZipFile(zip)) {
-          Enumeration<? extends ZipEntry> entries = zipFile.entries();
-          while (entries.hasMoreElements()) {
-              final ZipEntry entry = entries.nextElement();
-              if (entry.isDirectory()) {
-                  continue;
-              }
+        System.out.println("Downloading phantomjs from " + url + "...");
 
-              File to = new File(toDir, entry.getName());
-              to.getParentFile().mkdirs();
+        File zipTemp = new File(targetZip.getAbsolutePath() + ".temp");
+        try {
+            zipTemp.getParentFile().mkdirs();
 
-              Files.copy(new InputSupplier<InputStream>() {
-                  @Override
-                  public InputStream getInput() throws IOException {
-                      return zipFile.getInputStream(entry);
-                  }
-              }, to);
-          }
-      }
+            InputSupplier<InputStream> input = Resources.newInputStreamSupplier(URI.create(url).toURL());
+            OutputSupplier<FileOutputStream> ouput = Files.newOutputStreamSupplier(zipTemp);
 
-  }
+            ByteStreams.copy(input, ouput);
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to download phantomjs from " + url);
+        }
+
+        zipTemp.renameTo(targetZip);
+    }
+
+    private static void unzip(File zip, File toDir) throws IOException {
+        try (ZipFile zipFile = new ZipFile(zip)) {
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                final ZipEntry entry = entries.nextElement();
+                if (entry.isDirectory()) {
+                    continue;
+                }
+
+                File to = new File(toDir, entry.getName());
+                to.getParentFile().mkdirs();
+
+                Files.copy(new InputSupplier<InputStream>() {
+                    @Override
+                    public InputStream getInput() throws IOException {
+                        return zipFile.getInputStream(entry);
+                    }
+                }, to);
+            }
+        }
+
+    }
 }
--- src/main/java/fr/ybonnel/simpleweb4j/test/phantomjs/PhantomJsTest.java
+++ src/main/java/fr/ybonnel/simpleweb4j/test/phantomjs/PhantomJsTest.java
@@ -17,64 +17,64 @@ import java.io.File;
 import static org.openqa.selenium.phantomjs.PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY;
 
 public abstract class PhantomJsTest extends FluentAdapter {
-  private static final Dimension DEFAULT_WINDOW_SIZE = new Dimension(1024, 768);
+    private static final Dimension DEFAULT_WINDOW_SIZE = new Dimension(1024, 768);
 
-  private static WebDriver driver;
+    private static WebDriver driver;
 
-  @Rule
-  public LifeCycle lifecycle = new LifeCycle();
+    @Rule
+    public LifeCycle lifecycle = new LifeCycle();
 
-  protected abstract String defaultUrl();
+    protected abstract String defaultUrl();
 
-  public Fluent goTo(String url) {
-    withDefaultUrl(defaultUrl());
-    return super.goTo(url);
-  }
+    public Fluent goTo(String url) {
+        withDefaultUrl(defaultUrl());
+        return super.goTo(url);
+    }
 
-  class LifeCycle extends TestWatcher {
-    @Override
-    protected void starting(Description description) {
-      if (null == driver) {
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-          @Override
-          public void run() {
-            if (driver != null) {
-              driver.quit();
+    class LifeCycle extends TestWatcher {
+        @Override
+        protected void starting(Description description) {
+            if (null == driver) {
+                Runtime.getRuntime().addShutdownHook(new Thread() {
+                    @Override
+                    public void run() {
+                        if (driver != null) {
+                            driver.quit();
+                        }
+                    }
+                });
+
+                driver = createDriver();
             }
-          }
-        });
 
-        driver = createDriver();
-      }
+            driver.manage().deleteAllCookies();
+            driver.manage().window().setSize(DEFAULT_WINDOW_SIZE);
+            initFluent(driver);
+        }
 
-      driver.manage().deleteAllCookies();
-      driver.manage().window().setSize(DEFAULT_WINDOW_SIZE);
-      initFluent(driver);
+        @Override
+        protected void succeeded(Description description) {
+            snapshotFile(description).delete();
+        }
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+            takeScreenShot(snapshotFile(description).getAbsolutePath());
+        }
+
+        private File snapshotFile(Description description) {
+            return new File("snapshots", description.getMethodName() + ".png");
+        }
+
+        private WebDriver createDriver() {
+            File phantomJsExe = new PhantomJsDownloader().downloadAndExtract();
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability(PHANTOMJS_EXECUTABLE_PATH_PROPERTY, phantomJsExe.getAbsolutePath());
+
+            DriverService service = PhantomJSDriverService.createDefaultService(capabilities);
+
+            return new PhantomJSDriver(service, capabilities);
+        }
     }
-
-    @Override
-    protected void succeeded(Description description) {
-      snapshotFile(description).delete();
-    }
-
-    @Override
-    protected void failed(Throwable e, Description description) {
-      takeScreenShot(snapshotFile(description).getAbsolutePath());
-    }
-
-    private File snapshotFile(Description description) {
-      return new File("snapshots", description.getMethodName() + ".png");
-    }
-
-    private WebDriver createDriver() {
-      File phantomJsExe = new PhantomJsDownloader().downloadAndExtract();
-
-      DesiredCapabilities capabilities = new DesiredCapabilities();
-      capabilities.setCapability(PHANTOMJS_EXECUTABLE_PATH_PROPERTY, phantomJsExe.getAbsolutePath());
-
-      DriverService service = PhantomJSDriverService.createDefaultService(capabilities);
-
-      return new PhantomJSDriver(service, capabilities);
-    }
-  }
 }
